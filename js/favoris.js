57f1001544e89bdc7ed177443b5de05df2faeda6
Aissam-salman <nasawi.lmj@gmail.com>
2023-06-02 16:05:35 +0200

    change fav page

--- js/favoris.js
+++ js/favoris.js
@@ -21,27 +21,27 @@ const voitures = [
   voitures.forEach(voiture => {
     // Créez les éléments HTML nécessaires pour la carte
     const carte = document.createElement("div");
-    const image = document.createElement("img");
-    image.setAttribute("src","")
-    const nom = document.createElement("h2");
-    nom.setAttribute("class","titre2 ")
     const prix = document.createElement("p");
     const caracteristiques = document.createElement("ul");
     const vitessemax = document.createElement("p");
     const performance = document.createElement("p")
+    const image = document.createElement("img");
+    image.setAttribute("src","")
+    const nom = document.createElement("h2");
+    nom.setAttribute("class","titre2 ")
 
 
     carte.classList.add("carte");
-    image.classList.add("carte-image");
-    nom.classList.add("carte-nom");
     prix.classList.add("carte-prix");
     caracteristiques.classList.add("carte-caracteristiques");
     vitessemax.classList.add("carte-vitessemax")
     performance.classList.add("carte-performance")
+    image.classList.add("carte-image");
+    nom.classList.add("carte-nom");
     // Définissez les valeurs et attributs des éléments HTML
     image.src = voiture.image;
     nom.textContent = voiture.nom;
-    prix.textContent = "Prix: " + voiture.prix + " $";
+    prix.textContent = "Prix: " + voiture.prix + " C";
   
     voiture.caracteristiques.forEach(caracteristique => {
       const li = document.createElement("li");
@@ -50,10 +50,10 @@ const voitures = [
     });
   
     // Ajoutez les éléments à la carte
-    carte.appendChild(image);
-    carte.appendChild(nom);
     carte.appendChild(prix);
     carte.appendChild(caracteristiques);
+    carte.appendChild(image);
+    carte.appendChild(nom);
   
     // Ajoutez la carte à l'élément conteneur
     carteContainer.appendChild(carte);
