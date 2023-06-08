b990e217b70f39390e5d75fa7a52bea7d068785c
Paramore Williams <deschutter.aurelien@bbox.fr>
2023-06-08 11:14:38 +0200

    modif pages voitures

--- js/FH js.js
+++ js/FH js.js
@@ -18,16 +18,13 @@ async function afficherModalJSON(carName) {
 
   // Créer une div pour le contenu de la modal
   var modalContentDiv = document.createElement("div");
-  modalContentDiv.style.backgroundColor = "turquoise";
+  modalContentDiv.style.backgroundColor = "#6BBCEE";
   modalContentDiv.style.color = "blue";
   modalContentDiv.style.textAlign = "center";
-  modalContentDiv.style.margin = "10% auto";
-  // modalContentDiv.style.marginTop= "px";
-  // modalContentDiv.style.marginBottom = "50px";
-  // modalContentDiv.style.marginRight = "50px";
-  // modalContentDiv.style.marginLeft = "50px";
-  modalContentDiv.style.padding = "10px";
-  modalContentDiv.style.width = "22%";
+  modalContentDiv.style.fontSize = "20px";
+  modalContentDiv.style.margin = "5% auto";
+  modalContentDiv.style.padding = "1px";
+  modalContentDiv.style.width = "23%";
 
   for (const car of cars) {
 
@@ -82,3 +79,48 @@ toutterrain : ${car.toutterrain}
 
 // Appeler la fonction pour afficher la modal
 
+function ajouterAuxFavoris(car) {
+  // Créer une div pour la modal
+  var modalDiv = document.createElement("div");
+  modalDiv.style.position = "fixed";
+  modalDiv.style.top = "200px";
+  modalDiv.style.left = "0";
+  modalDiv.style.width = "100%";
+  modalDiv.style.height = "100%";
+  modalDiv.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  modalDiv.style.zIndex = "9999";
+
+  // Créer une div pour le contenu de la modal
+  var modalContentDiv = document.createElement("div");
+  modalContentDiv.style.backgroundColor = "#6BBCEE"
+  modalContentDiv.style.color = "blue";
+  modalContentDiv.style.textAlign = "center";
+  modalContentDiv.style.fontSize = "13px";
+  modalContentDiv.style.margin = "5% auto";
+  modalContentDiv.style.padding = "5px";
+  modalContentDiv.style.width = "20%";
+
+  var jsonPre = document.createElement("pre");
+  jsonPre.innerText = `votre véhicule à bien été ajouté aux favoris`;
+  // Ajouter le pre à la div de contenu de la modal
+  modalContentDiv.appendChild(jsonPre);
+
+  // Créer un bouton de fermeture de la modal
+  var closeButton = document.createElement("button");
+  closeButton.textContent = "Fermer";
+  closeButton.style.marginTop = "10px";
+
+  // Ajouter un gestionnaire d'événement pour fermer la modal lorsqu'on clique sur le bouton
+  closeButton.addEventListener("click", function () {
+    modalDiv.remove();
+  });
+
+  // Ajouter le bouton de fermeture à la div de contenu de la modal
+  modalContentDiv.appendChild(closeButton);
+
+  // Ajouter la div de contenu de la modal à la div de la modal
+  modalDiv.appendChild(modalContentDiv);
+
+  // Ajouter la div de la modal au body du document
+  document.body.appendChild(modalDiv);
+}
